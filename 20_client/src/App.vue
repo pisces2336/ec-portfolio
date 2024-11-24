ac632b992ff59341a2cea51a998a8fe84863f735
pisces2336 <pisces2336@gmail.com>
2024-11-24 19:30:42 +0900

    feat: ログイン

--- 20_client/src/App.vue
+++ 20_client/src/App.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import axios from 'axios'
+import { RouterView, useRoute, useRouter } from 'vue-router'
+import { useCookies } from 'vue3-cookies'
+
+const { cookies } = useCookies()
+const accessToken = cookies.get('accessToken')
+const router = useRouter()
+const route = useRoute()
+
+// token検証
+axios.post('api/jwt/verify', { token: accessToken }).catch(() => {
+  if (route.path !== '/login') {
+    alert('ログインが必要です。')
+    router.push('login')
+  }
+})
 </script>
 
 <template>
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ 20_client/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import LoginForm from '@/components/LoginForm.vue'
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <LoginForm />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
