89f1b910e3cbc29a7983f8504aadabab0a465653
pisces2336 <pisces2336@gmail.com>
2024-11-24 19:45:55 +0900

    feat: Itemモデル作成

--- 30_server/shop/models.py
+++ 30_server/shop/models.py
@@ -1,3 +1,11 @@
+from core.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Item(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    class Meta:
+        db_table = "item"
